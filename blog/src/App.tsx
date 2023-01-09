c2be497fc1de6dfe7179b620ce78cb0093ae7832
jb <j_d_barker@hotmail.co.uk>
2023-01-09 14:46:47 +0000

    finished fixing

--- blog/src/App.tsx
+++ blog/src/App.tsx
@@ -8,35 +8,40 @@ import "./styling/global.css";
 function App() {
   const [isAuth, setIsAuth] = useState(false);
   return (
-    <div className="App">
-      <main>
-        <nav>
-          <Link to={"/"}>
-            <h1>dev-blog</h1>
-          </Link>
-          <Link to={"/new-post"}>
-            <button className="new-post">+</button>
-          </Link>
-          <Login isAuth={isAuth} setIsAuth={setIsAuth} />
-        </nav>
+    <div className="container">
+      <div className="gradient">
 
-        <Routes>
-          <Route path="/" element={<Posts />} />
-          <Route path="/new-post" element={<NewPost />} />
-        </Routes>
-      </main>
-      <footer>
-        <h3>
-          developed by{" "}
-          <a
-            href="https://github.com/jonny-barker"
-            target="_blank"
-            rel="noreferrer"
-          >
-            jonny barker
-          </a>
-        </h3>
-      </footer>
+      </div>
+      <div className="App">
+        <main>
+          <nav>
+            <Link to={"/"}>
+              <h1>dev-blog</h1>
+            </Link>
+            <Link to={"/new-post"}>
+              <button className="new-post">+</button>
+            </Link>
+            <Login isAuth={isAuth} setIsAuth={setIsAuth} />
+          </nav>
+
+          <Routes>
+            <Route path="/" element={<Posts />} />
+            <Route path="/new-post" element={<NewPost />} />
+          </Routes>
+        </main>
+        <footer>
+          <h3>
+            developed by{" "}
+            <a
+              href="https://github.com/jonny-barker"
+              target="_blank"
+              rel="noreferrer"
+            >
+              jonny barker
+            </a>
+          </h3>
+        </footer>
+      </div>
     </div>
   );
 }
--- blog/src/components/Posts.tsx
+++ blog/src/components/Posts.tsx
@@ -4,67 +4,29 @@ import { getDocs, collection } from "firebase/firestore";
 import { useEffect, useState } from "react";
 
 export const Posts = () => {
-  const [posts, setPosts] = useState<any>([
-    {
-      id: 1672408216543,
-      subTitle: "",
-      title: "test3",
-      body: "",
-    },
-    {
-      title: "test",
-      body: "",
-      subTitle: "",
-      id: 1672403727055,
-    },
-    {
-      body: "",
-      subTitle: "",
-      title: "test2",
-      id: 1672408214474,
-    },
-    {
-      title: "test 5",
-      subTitle: "this is a test",
-      id: 1672744047906,
-      body: "hopefully this one works",
-    },
-    {
-      subTitle: "",
-      title: "test1",
-      id: 1672408212694,
-      body: "",
-    },
-    {
-      id: 1672408219600,
-      body: "",
-      title: "test4",
-      subTitle: "",
-    },
-  ]);
-
+  const [posts, setPosts] = useState<any>([]);
 
   const [loading, setLoading] = useState(false);
 
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     try {
-  //       const docRef = collection(db, "posts");
-  //       const response = await getDocs(docRef);
-  //       let data: any = [];
-  //       if (response) {
-  //         response.forEach((doc) => {
-  //           data.push(doc.data());
-  //         });
-  //       }
-  //       setPosts(data);
-  //       setLoading(false);
-  //     } catch (err) {
-  //       console.error(err);
-  //     }
-  //   };
-  //   fetchData();
-  // }, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const docRef = collection(db, "posts");
+        const response = await getDocs(docRef);
+        let data: any = [];
+        if (response) {
+          response.forEach((doc) => {
+            data.push(doc.data());
+          });
+        }
+        setPosts(data);
+        setLoading(false);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchData();
+  }, [setPosts, setLoading]);
 
   console.log(posts);
   return loading ? (
@@ -72,9 +34,10 @@ export const Posts = () => {
   ) : (
     <div className="posts">
       {posts.map((post: any) => {
+        const date = new Date(post.id).toUTCString();
         return (
-          <div key={post.id}>
-            <h4 className="id">{post.id}</h4>
+          <div key={post.id} className="post">
+            <h4 className="id">{date}</h4>
             <h2 className="title">{post.title}</h2>
             <h3 className="sub-title">{post.subTitle}</h3>
             <p className="body">{post.body}</p>
